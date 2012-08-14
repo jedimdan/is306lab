f0e879eacd5d90e73de5bd8772932ed9988e3faf
Daniel <daniel.tsou.2009@smu.edu.sg>
2012-08-14 23:47:59 +0800

    Modified the header text

--- php/index.php
+++ php/index.php
@@ -121,7 +121,7 @@
     <div class="brand-text"><strong>Open</strong>Shift</div>
   </a>
   <h1>
-    Welcome to OpenShift
+    Welcome to OpenShift, IS306!
   </h1>
   <p>
     Place your application here
